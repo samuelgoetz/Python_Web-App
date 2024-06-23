efd89db4875cd754523fe4f86904c5b5fe2a892b
Jaxon412 <management.samuel.goetz@gmail.com>
2024-06-23 14:17:00 +0200

    inital

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def hello():
     name = request.form.get('name')
     # Definiere eine Liste gültiger Eingaben
     valid_inputs = ["1 und 2", "2 und 1", "1,2", "2,1", "2&1", "1&2"]
-    # valid_inputs = ["1 und 2", "2 und 1", "1,2", "2,1", "2&1", "1&2", "1 2", "12", "1/2"]
+    # valid_inputs = ["1 und 2", "2 und 1", "1,2", "2,1", "2&1", "1&2", "1 2", "2 1", "12", "21", "1/2", "2/1"]
  
     # Überprüfe, ob die Eingabe in der Liste gültiger Eingaben ist
     if name and any(name.lower() == valid.lower() for valid in valid_inputs):
@@ -28,7 +28,7 @@ def hello():
         return render_template('hello.html', name=name)
     else:
         print('Tja Thaddaus, war wohl nix!')
-        return redirect(url_for('index'))
+        return render_template('nothello.html', name=name)
 
 
 if __name__ == '__main__':
